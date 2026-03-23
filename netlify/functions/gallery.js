ca9ee36e7fc6ed5e6295bb4af8c9fb644a6e7dcd
mvmags <marco.velasco@gmail.com>
2026-03-22 21:15:47 -0600

    updated the gallery function

--- netlify/functions/gallery.js
+++ netlify/functions/gallery.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+const path = require("path");
+
 exports.handler = async (event) => {
   if (event.httpMethod !== "GET") {
     return {
@@ -7,9 +10,32 @@ exports.handler = async (event) => {
     };
   }
 
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "text/html" },
-    body: '<img src="images/project1/test.jpg" alt="image gallery" class="carousel-slide">'
-  };
+  try {
+    const folderPath = path.join(process.cwd(), "images", "project1");
+    const files = fs.readdirSync(folderPath);
+
+    const allowedExtensions = [".jpg", ".jpeg", ".png", ".webp", ".gif"];
+
+    const html = files
+      .filter((file) => {
+        const ext = path.extname(file).toLowerCase();
+        return allowedExtensions.includes(ext);
+      })
+      .map((file) => {
+        return `<img src="images/project1/${file}" alt="image gallery" class="carousel-slide">`;
+      })
+      .join("\n");
+
+    return {
+      statusCode: 200,
+      headers: { "Content-Type": "text/html" },
+      body: html
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers: { "Content-Type": "text/plain" },
+      body: `Error reading gallery folder: ${error.message}`
+    };
+  }
 };
